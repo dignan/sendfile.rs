fe5417177d092d70a9f83f38b1f8f482b36c5924
Patrick <dignan.patrick@gmail.com>
2015-10-20 18:45:48 -0400

    Attempt to connect to the test server a few times

--- tests/lib.rs
+++ tests/lib.rs
@@ -13,8 +13,8 @@ fn test_sendfile() {
     let test_string = String::from("Hello tests");
 
     // Accept the connection that will come in in another thread
-    let listening_thread = thread::spawn(|| {
-        return listen_for_sendfile(9007u16);
+    let listening_thread = thread::spawn(move|| {
+        return listen_for_sendfile(9009u16);
     });
 
     // Create a file and fill it with some data
@@ -29,15 +29,37 @@ fn test_sendfile() {
 
     // Call the Mac OS sendfile implementation
     let source = File::open("/tmp/testsendfile").unwrap();
-    let sink = TcpStream::connect("127.0.0.1:9007").unwrap();
+    let mut sink_maybe = None;
+    let mut connected = false;
+    let mut tries = 0;
 
-    println!("Connected to {} as {}", sink.peer_addr().unwrap(), sink.local_addr().unwrap());
-    let sendfile_bytes_sent = sendfile::sendfile(&source, &sink, 0, bytes_len as i64).unwrap();
-    println!("Sent {} bytes using sendfile", sendfile_bytes_sent);
+    while !connected && tries < 5{
+        println!("connected={}", connected);
+        match TcpStream::connect("127.0.0.1:9009") {
+            Ok(stream) => {
+                sink_maybe = Some(stream);
+                connected = true;
+            }
+            Err(e) => {
+                tries += 1;
+                println!("Couldn't connect: {}", e);
+                thread::sleep_ms(100);
+            }
+        }
+    }
 
-    let received_string = listening_thread.join().unwrap();
-    println!("Got '{}'", &received_string);
-    assert_eq!(received_string, test_string);
+    match sink_maybe {
+        Some(sink) => {
+            println!("Connected to {} as {}", sink.peer_addr().unwrap(), sink.local_addr().unwrap());
+            let sendfile_bytes_sent = sendfile::sendfile(&source, &sink, 0, bytes_len as i64).unwrap();
+            println!("Sent {} bytes using sendfile", sendfile_bytes_sent);
+
+            let received_string = listening_thread.join().unwrap();
+            println!("Got '{}'", &received_string);
+            assert_eq!(received_string, test_string);
+        }
+        None => panic!("Couldn't connect to the test server")
+    }
 }
 
 #[test]
@@ -45,7 +67,7 @@ fn test_sendfile_with_offset() {
     let test_string = String::from("Hello tests");
 
     // Accept the connection that will come in in another thread
-    let listening_thread = thread::spawn(|| {
+    let listening_thread = thread::spawn(move|| {
         return listen_for_sendfile(9006u16);
     });
 
@@ -61,22 +83,46 @@ fn test_sendfile_with_offset() {
 
     // Call the Mac OS sendfile implementation
     let source = File::open("/tmp/testsendfile").unwrap();
-    let sink = TcpStream::connect("127.0.0.1:9006").unwrap();
 
-    println!("Connected to {} as {}", sink.peer_addr().unwrap(), sink.local_addr().unwrap());
-    let sendfile_bytes_sent = sendfile::sendfile(&source, &sink, 3, bytes_len as i64).unwrap();
-    println!("Sent {} bytes using sendfile", sendfile_bytes_sent);
+    let mut sink_maybe = None;
+    let mut connected = false;
+    let mut tries = 0;
 
-    let received_string = listening_thread.join().unwrap();
-    println!("Got '{}'", &received_string);
+    while !connected && tries < 5{
+        println!("connected={}", connected);
+        match TcpStream::connect("127.0.0.1:9006") {
+            Ok(stream) => {
+                sink_maybe = Some(stream);
+                connected = true;
+            }
+            Err(e) => {
+                tries += 1;
+                println!("Couldn't connect: {}", e);
+                thread::sleep_ms(100);
+            }
+        }
+    }
 
-    assert_eq!(received_string, (&test_string[3..]));
+    match sink_maybe {
+        Some(sink) => {
+            println!("Connected to {} as {}", sink.peer_addr().unwrap(), sink.local_addr().unwrap());
+            let sendfile_bytes_sent = sendfile::sendfile(&source, &sink, 3, bytes_len as i64).unwrap();
+            println!("Sent {} bytes using sendfile", sendfile_bytes_sent);
+
+            let received_string = listening_thread.join().unwrap();
+            println!("Got '{}'", &received_string);
+
+            assert_eq!(received_string, (&test_string[3..]));
+        }
+        None => panic!("Couldn't connect to the test server")
+    }
 }
 
 fn listen_for_sendfile(port: u16) -> String {
     // Create the socket to listen on first
     let listener = TcpListener::bind(("127.0.0.1", port)).unwrap();
     let stream_and_addr = listener.accept().unwrap();
+
     let mut stream = stream_and_addr.0;
     let addr = stream_and_addr.1;
 
